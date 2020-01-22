d9a37c4b9093983500925cac21eaf61f10c836db
Pablo <pabloriverar.sv@gmail.com>
2020-01-22 10:15:55 -0600

    commit inicial

---
+++ app.js
@@ -0,0 +1,39 @@
+const argv = require('./config/yargs').argv;
+const colors = require('colors/safe');
+
+
+const { crearArchivo, listarTabla } = require('./multiplicar/multiplicar');
+
+let comando = argv._[0];
+
+switch (comando) {
+
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${archivo.green}`))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+
+}
+
+
+//console.log(process);
+
+//let argv2 = process.argv;
+
+//console.log(argv.base);
+
+
+//let parametro = argv[2];
+//let base = parametro.split('=')[1]
+
+//crearArchivo(base)
+//  .then(archivo => console.log(`Archivo creado: ${archivo}`))
+//.catch(e => console.log(e));
